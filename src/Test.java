459e2eb0e746bf757e38ae1b4431d9ae463287e0
Eric <378240631@qq.com>
2022-05-30 11:58:38 +0800

    m1

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: Eric
+ * @Date: 2022/5/30 - 05 - 30 - 11:57
+ * @Description: Let's go!
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("mastet01");
+    }
+}
